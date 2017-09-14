ef93d6927f2059cfaeabf9f1b197e3a38cd1efe6
Artem Metelskyi <metelskyi@Artems-MBP.fios-router.home>
2017-09-14 13:35:13 -0400

    Masha kurit

--- Whatever/ViewController.swift
+++ Whatever/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Masha kurit sigaretu")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
